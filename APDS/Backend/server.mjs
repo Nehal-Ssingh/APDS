e39eb5e64bec702c03d36e91021731b872af213c
Nehal3010 <151540482+Nehal-Ssingh@users.noreply.github.com>
2024-07-29 09:48:25 +0200

    29July2024

--- APDS/Backend/server.mjs
+++ APDS/Backend/server.mjs
@@ -1,1 +1,12 @@
-console.log('Hello');
+import https from "https";
+import fs from "fs";
+
+
+const PORT =3000;
+
+const server = https.createServer({
+key: fs.readFileSync('keys/privatekey.pem'),
+cert: fs.readFileSync('keys/certificate.pem')
+})
+
+server.listen(PORT);
