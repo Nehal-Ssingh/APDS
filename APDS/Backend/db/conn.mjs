af953279e4d6d0b92d9184472e56a9ca10230186
Nehal3010 <151540482+Nehal-Ssingh@users.noreply.github.com>
2024-08-12 10:17:04 +0200

    Backend Complete

---
+++ APDS/Backend/db/conn.mjs
@@ -0,0 +1,22 @@
+import {MongoClient} from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectionstring = process.env.ATLAS_URI || "";
+
+console.log(connectionstring);
+
+const client = new MongoClient(connectionstring);
+
+let conn;
+try
+{
+conn = await client.connect();
+console.log('mongoDB is connected !!!!!! :)')
+}catch(e){
+console.error(e);
+}
+
+let db = client.db("users");
+
+export default db;
--- APDS/Backend/server.mjs
+++ APDS/Backend/server.mjs
@@ -1,12 +1,35 @@
 import https from "https";
+import http from "http";
 import fs from "fs";
+import posts from "./routes/post.mjs";
+import users from "./routes/user.mjs"
+import express from "express";
+import cors from "cors";
+import brute from "express-brute";
 
+const PORT = 3001;
+const app = express();
 
-const PORT =3000;
+const options = {
+    key: fs.readFileSync('Keys/privatekey.pem'),
+    cert: fs.readFileSync('Keys/certificate.pem')
+};
 
-const server = https.createServer({
-key: fs.readFileSync('keys/privatekey.pem'),
-cert: fs.readFileSync('keys/certificate.pem')
-})
+app.use(cors());
+app.use(express.json());
+
+app.use((reg, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    next();
+});
+
+app.use("/post", posts);
+app.route("/post", posts);
+app.use("/user", users);
+app.use("/user", users);
+
+let server = https.createServer(options, app);
 
 server.listen(PORT);
